f3dd0fa32dc3b66cf2e40f36a7976069603acbf6
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-01-22 21:43:50 +0000

    Note Installe

---
+++ installing/ArchLinuxInstaller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Installing Note"
+x=`pacman -Qs go`
+if [ -n "$x" ]
+then
+    touch note.go
+    touch go.mod
+    curl https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/note.go > note.go
+    curl https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/go.mod > go.mod
+    go build note.go
+    sudo cp note /usr/bin
+    rm note.go
+    rm go.mod
+else
+    sudo pacman -S go
+    touch note.go
+    touch go.mod
+    curl https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/note.go > note.go
+    curl https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/go.mod > go.mod
+    go build note.go
+    sudo cp note /usr/bin
+    rm note.go
+    rm go.mod
+fi
---
+++ installing/install.md
@@ -0,0 +1,15 @@
+# Installing
+___
+
+Installing Note is a really easy task to do!
+I made a quick little script to install the latest version of Note for anyone who wishes to use note but doesn't know how to install with source code!
+First off to start you are going to want to run these commands in your terminal
+```shell
+touch installer.sh
+// Replace the raw file with your Linux Operating system.
+// For this example we will use the ArchLinux Installer
+curl https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/installing/ArchLinuxInstaller.sh > installer.sh
+// Run this to allow executing file as program
+sudo chmod u+x installer.sh
+```
+After you are done running these commands in your terminal of choice,z
