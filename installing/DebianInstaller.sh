220abadc4bf035b62734a9717aa94c45e2838fe6
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-01-23 03:30:24 +0000

    Update DebianInstaller.sh

--- installing/DebianInstaller.sh
+++ installing/DebianInstaller.sh
@@ -16,11 +16,12 @@ else
 if  [ $request == "y" ]
 then
 # Installs Golang 1.17
-wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
 tar -xzvf go1.17.6.linux-amd64.tar.gz
 sudo mv go /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
+rm go1.17.6.linux-amd64.tar.gz
 # Grabs Newest version of Note
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/go.mod
