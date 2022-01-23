e67c36b79b6da671eb2b403bd873fb50371dc1ea
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-01-23 04:06:43 +0000

    Bug Fix #2

--- installing/ArchLinuxInstaller.sh
+++ installing/ArchLinuxInstaller.sh
@@ -3,7 +3,6 @@ echo "Installing Note"
 x=`pacman -Qs go`
 if [ -n "$x" ]
 then
-    sudo pacman -S go
     touch note.go
     touch go.mod
     // Grabs Newest version of Note
@@ -14,6 +13,7 @@ then
     rm note.go
     rm go.mod
 else
+    sudo pacman -S go
     touch note.go
     touch go.mod
     // Grabs Newest version of Note
