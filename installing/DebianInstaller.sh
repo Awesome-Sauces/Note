75d47f282075f2237033eaaf7905dd3047db80b2
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-01-23 03:26:27 +0000

    Bug fix on DebianInstaller

--- installing/DebianInstaller.sh
+++ installing/DebianInstaller.sh
@@ -4,24 +4,24 @@ which $pkg > /dev/null 2>&1
 if [ $? == 0 ]
 then
 ls
-// Grabs Newest version of Note
+# Grabs Newest version of Note
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/note.go > note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/go.mod > go.mod
 go build note.go
 sudo cp note /usr/bin
-// rm note.go
-// rm go.mod
+rm note.go
+rm go.mod
 else
     read -p "$pkg is not installed. Would you like to install it? y/n " request
 if  [ $request == "y" ]
 then
-// Installs Golang 1.17
+# Installs Golang 1.17
 wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
 tar -xzvf go1.17.6.linux-amd64.tar.gz
 sudo mv go /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
-// Grabs Newest version of Note
+# Grabs Newest version of Note
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/Note-v1.0.0-BETA/src/go.mod
 go build note.go
