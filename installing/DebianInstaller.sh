c17096aae897b7110acd45daf677b505c9ae9eff
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-01-25 06:12:34 +0000

    Fixed DebianInstaller Bug added some goodies too note.go and began work on colorConfig.json

--- installing/DebianInstaller.sh
+++ installing/DebianInstaller.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pkg="go"
+u="$USER"
 which $pkg > /dev/null 2>&1
 if [ $? == 0 ]
 then
@@ -9,18 +10,19 @@ wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.mod
 go build note.go
 sudo mv note /usr/bin
-rm note.go
-rm go.mod
 else
-    read -p "$pkg is not installed. Would you like to install it? y/n " request
+    read -p "Golang is not installed. Would you like to install it? y/n : " request
 if  [ $request == "y" ]
 then
 # Installs Golang 1.17
-wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
-tar -xzvf go1.17.6.linux-amd64.tar.gz
-sudo mv go /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-source ~/.bashrc
+(cd /tmp; wget https://golang.org/dl/go1.17.linux-amd64.tar.gz)
+(cd /tmp; tar -xzvf go1.17.linux-amd64.tar.gz)
+(cd /tmp; sudo mv go /usr/local)
+(cd /tmp; rm -rf go)
+echo 'export GOROOT=/usr/local/go' >> /home/$u/.bashrc
+echo 'export GOPATH=$HOME/go' >> /home/$u/.bashrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> /home/$u/.bashrc
+source /home/$u/bashrc
 # Grabs Newest version of Note
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.mod
