1a97455bf87fd46d62a769aacd556ba353493ded
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-08-26 05:27:34 +0000

    Update ubuntu.sh

--- installers/ubuntu.sh
+++ installers/ubuntu.sh
@@ -18,6 +18,10 @@ then
 fi
 fi
 
+sudo mkdir ~/note
+touch theme.rocky
+sudo mv -i themes.rocky ~/note
+
 # Grabs Newest version of Note
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/note.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/structs.go
@@ -26,11 +30,16 @@ wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/commands.go
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.mod
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.sum
 wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/build.sh
+wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/TextEditor.go
+wget https://raw.githubusercontent.com/Awesome-Sauces/Note/main/themes/theme.rocky
 sh build.sh
 sudo mv -i note /usr/bin
+sudo note -deploy theme.rocky
 rm note.go
 rm structs.go
 rm config.go
+rm TextEditor.go
+rm theme.rocky
 rm commands.go
 rm go.sum
 rm go.mod
