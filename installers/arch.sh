ee8be04dfaaec3e8d92d3cf09f5c77fa47d17668
Awesome Sauce <78565561+Awesome-Sauces@users.noreply.github.com>
2022-08-26 05:25:53 +0000

    Update arch.sh

--- installers/arch.sh
+++ installers/arch.sh
@@ -10,6 +10,10 @@ then
 fi
 fi
 
+sudo mkdir ~/note
+touch theme.rocky
+sudo mv -i themes.rocky ~/note
+
 touch note.go
 touch structs.go
 touch config.go
@@ -17,23 +21,25 @@ touch commands.go
 touch build.sh
 touch go.sum
 touch go.mod
+touch TextEditor.go
 # Grabs Newest version of Note
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/note.go > note.go
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/structs.go > structs.go
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/config.go > config.go
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/commands.go > commands.go
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.mod > go.mod
+curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/TextEditor.go > TextEditor.go
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/go.sum > go.sum
 curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/build.sh > build.sh
+curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/themes/theme.rocky > theme.rocky
 sh build.sh
 sudo mv -i note /usr/bin
+sudo note -deploy theme.rocky
 rm note.go
 rm structs.go
 rm config.go
 rm commands.go
 rm go.sum
 rm go.mod
+rm theme.rocky
 rm build.sh
-sudo mkdir ~/note
-curl https://raw.githubusercontent.com/Awesome-Sauces/Note/main/src/colorConfig.json > colorConfig.json
-sudo mv -i colorConfig.json ~/note
